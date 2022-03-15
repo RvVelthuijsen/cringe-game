9e48a448b8344d9105e9c8f1c34b31b40435c406
Trang <ttphan.contact@gmail.com>
2022-03-15 17:27:21 +0100

    creating stats page, incl. css and js file

---
+++ stats.js
@@ -0,0 +1,14 @@
+// Changing the stats message
+const statsMessage = document.querySelector(.stats-message);
+
+/*if (player wins) {
+    statsMessage.innerHTML = "Congrats! You did it!";
+}
+else if (player loses) {
+    statsMessage.innerHTML = "Oops! You failed!";
+}
+*/
+
+// Adding rows to the table
+
+const playersTableRow = document.createElement("tr")
