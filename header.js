10bc0f57ac381edc37f07de081db6845a3f82279
Gilberto Mascarenhas <gilbertomascarenhas@Gilbertos-MacBook-Pro-2.local>
2022-03-16 16:46:40 +0100

    overhaul

--- header.js
+++ header.js
@@ -12,3 +12,18 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     burgerMenu.addEventListener("click", toggle);
 });
+
+// Changing the stats message"
+const statsMessage = document.querySelector(".stats-message");
+
+/*if (player wins) {
+    statsMessage.innerHTML = "Congrats! You did it!";
+}
+else if (player loses) {
+    statsMessage.innerHTML = "Oops! You failed!";
+}
+*/
+
+// Adding rows to the table
+
+const playersTableRow = document.createElement("tr");
