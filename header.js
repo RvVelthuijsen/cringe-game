868d07a544041103436d8581c8ce18e1db7842a9
Gilberto Mascarenhas <gilbertomascarenhas@Gilbertos-MacBook-Pro-2.local>
2022-03-16 09:12:34 +0100

    burger button fixed

--- header.js
+++ header.js
@@ -1,11 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.getElementById("burger-menu");
-    const navBar = document.getElementsByClassName("nav-link");
+    const navBar = document.getElementById('navbar');
+    console.log(navBar);
     let isOpen = false;    
-    const toggle = () => {
-        if (isOpen === false) {
-            navBar.style.visibility = "visible"
-        } else {navBar.style.visibility = "hidden"}
-        // isOpen ? navBar.style.visibility = "hidden" : navBar.style.visibility = "visible";
+    function toggle() {
+        // if (isOpen === false) {
+        //     navBar.style.visibility = "visible"
+        // } else {navBar.style.visibility = "hidden"}
+        isOpen ? navBar.style.visibility = "hidden" : navBar.style.visibility = "visible";
         isOpen = !isOpen;
     };
     burgerMenu.addEventListener("click", toggle);
+});
