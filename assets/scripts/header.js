3a5dc28a1a9d3854ec4757694e172c56326cee3a
RvVelthuijsen <r.v.velthuijsen@gmail.com>
2022-03-23 16:04:55 +0100

    wrapped code for individual pages in if statements

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -60,58 +60,63 @@ toggleBtn.addEventListener("click", toggleDark);
 
 // FORM VALIDATION (on impressum page)
 
-const submitBtn = document.querySelector(".submit-btn");
-const submitMsg = document.querySelector(".submit-msg");
-const form = document.querySelector(".contact-form");
+if (window.location.href === "./impressum.html") {
+  const submitBtn = document.querySelector(".submit-btn");
+  const submitMsg = document.querySelector(".submit-msg");
+  const form = document.querySelector(".contact-form");
 
-function validateForm() {
-  const inputFields1 = document.forms["form"]["name"].value;
-  const inputFields2 = document.forms["form"]["email"].value;
-  const inputFields3 = document.forms["form"]["message"].value;
+  function validateForm() {
+    const inputFields1 = document.forms["form"]["name"].value;
+    const inputFields2 = document.forms["form"]["email"].value;
+    const inputFields3 = document.forms["form"]["message"].value;
 
-  if (!inputFields1 || !inputFields2 || !inputFields3) {
-    submitMsg.innerHTML = "Oops! Please fill out all fields before submitting!";
-    /* return false; */
-  } else {
-    submitMsg.innerHTML = "Great! Thank you for your message!";
-    submitMsg.classList.add("success-msg");
-    form.setAttribute("id", "form-submitted");
+    if (!inputFields1 || !inputFields2 || !inputFields3) {
+      submitMsg.innerHTML =
+        "Oops! Please fill out all fields before submitting!";
+      /* return false; */
+    } else {
+      submitMsg.innerHTML = "Great! Thank you for your message!";
+      submitMsg.classList.add("success-msg");
+      form.setAttribute("id", "form-submitted");
+    }
   }
-}
 
-submitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  validateForm();
-});
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    validateForm();
+  });
+}
 
 // DISPLAY STATS FROM LOCAL STORAGE
 
-let results = localStorage.getItem("results");
-let currentResults = JSON.parse(results);
-console.log(currentResults);
+if (window.location.href === "./stats.html") {
+  const statsTable = document.querySelector(".stats-table");
+  const noResultsRow = document.querySelector(".no-results-row");
 
-const statsTable = document.querySelector(".stats-table");
-const noResultsRow = document.querySelector(".no-results-row");
+  // checking if results is not empty, then remove the test row
+  if (localStorage.getItem("results")) {
+    noResultsRow.remove();
 
-if (currentResults) {      // checking if results is not empty, then remove the test row
-  noResultsRow.remove();
+    let results = localStorage.getItem("results");
+    let currentResults = JSON.parse(results);
+    console.log(currentResults);
 
-  for (let i=0; i<currentResults.length; i++) {    // looping over number ob objects in results array
+    for (let i=0; i<currentResults.length; i++) {    // looping over number ob objects in results array
 
-  var tableRow[i] = document.createElement("tr");   // creating and appending table rows for each object in array
-  statsTable.appendChild(tableRow[i]);
+    var tableRow[i] = document.createElement("tr");   // creating and appending table rows for each object in array
+    statsTable.appendChild(tableRow[i]);
 
-  var tableCell1[i] = document.createElement("td");   // creating and appending table cells for each object, using "var" instead of "let" because of error message on scope
-  var tableCell2[i] = document.createElement("td");
-  var tableCell3[i] = document.createElement("td");
-  var tableCell4[i] = document.createElement("td");
+    var tableCell1[i] = document.createElement("td");   // creating and appending table cells for each object, using "var" instead of "let" because of error message on scope
+    var tableCell2[i] = document.createElement("td");
+    var tableCell3[i] = document.createElement("td");
+    var tableCell4[i] = document.createElement("td");
 
-  tableRow[i].appendChild(tableCell1[i], tableCell2[i], tableCell3[i], tableCell4[i]); // check if this works, otherwise only "append"
+    tableRow[i].appendChild(tableCell1[i], tableCell2[i], tableCell3[i], tableCell4[i]); // check if this works, otherwise only "append"
 
-  tableCell1[i].innerHTML = results[i].name;    // modify content of table cells
-  tableCell2[i].innerHTML = results[i].timeLeft;
-  tableCell3[i].innerHTML = results[i].numPickups;
-  tableCell4[i].innerHTML = results[i].score;
+    tableCell1[i].innerHTML = results[i].name;    // modify content of table cells
+    tableCell2[i].innerHTML = results[i].timeLeft;
+    tableCell3[i].innerHTML = results[i].numPickups;
+    tableCell4[i].innerHTML = results[i].score;
+    }
   }
 }
-
